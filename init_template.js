b5715b401d05a8b85caa1f0cd5a8d6f953442369
Moonsung <gmsj159@gmail.com>
2021-05-23 18:36:55 +1000

    init_template

---
+++ init_template.js
@@ -0,0 +1,26 @@
+const init = {
+    connect:{
+        hostname:'',
+        port:'',
+        address:''
+    },
+    db:{
+        host:'',
+        user:'',
+        password:'',
+        database:'',
+    },
+    email:{
+        address:'',
+        clientId:'',
+        clientSecret:'',
+        refreshToken:'',
+        accessToken:''
+    },
+    admin:{
+        code:95981
+    }
+    // admin ID: admin
+    // admin Password: 111111
+}
+module.exports = init;
